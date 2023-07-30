4dce2fb984ba8a1215d6654a5431e615c5c6a931
Maitham Al-rubaye <maythem.altamar@gmail.com>
2023-07-30 19:57:48 +0200

    CSharp, C#, c# , cs, syntax, Csharp, tutorial

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace CSharpTutorial
 {
-     class Program
+    class Program
     {
         static void Main(string[] args)
         {
@@ -18,7 +18,60 @@ namespace CSharpTutorial
             {
                 Console.WriteLine(number);
             }
+
+            // arrays
+            int[,,] array1 = new int[2, 2, 2] { 
+                { { 1, 2 }, { 3, 4 } }, 
+                { { 5, 6 }, { 7, 8 } }
+                };
+
+            for (int i=0; i < array1.GetLength(0); i++)
+            {
+                for (int j=0; j < array1.GetLength(1); j++)
+                {
+                    for (int k=0; k < array1.GetLength(2); k++)
+                    {
+                        Console.WriteLine(array1[i, j, k]);
+                    }
+                }
+            }
+
+            int[,] arrayA = new int[,]
+            {
+                {1, 1, 1},
+                {1, 1, 1},
+                {1, 1, 1}
+            };
+
+            int[,] arrayB = new int[,]
+            {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+            };
+
+            int[,] arrayC = new int[3, 3];
+
+            for (int i = 0; i < arrayA.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayA.GetLength(1); j++)
+                {
+                    arrayC[i, j] = arrayA[i, j] * arrayB[i, j];
+                }
+            }
+
+            for (int i = 0; i < arrayC.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayC.GetLength(1); j++)
+                {
+                    Console.Write(arrayC[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 
 }
+
+// here we are using the LINQ query syntax to select all numbers in the list that are
+// divisible by 3 and then we are printing them out to the console.
